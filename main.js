7dc5cec6ec35cbe371052efb576f443a957ea420
Jonathan Vargas <jonathanv3232@gmail.com>
2017-12-29 18:49:55 -0500

    enables type ahead

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+let cities = [];
+
+const prom = fetch(endpoint)
+  .then(stream => stream.json())
+  .then(data => {
+    cities.push(...data);
+  });
+
+function findMatches(searchTerm, data) {
+  const regex = new RegExp(searchTerm, 'gi');
+
+  const matches = cities.filter(place => place.city.match(regex) || place.state.match(regex));
+  return matches;
+}
+
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+function displayMatches() {
+  const matches = findMatches(this.value, cities);
+  const html = matches.map(
+    place => `
+    <li>
+      <span class='name'>${place.city}, ${place.state}</span>
+    </li>
+  `,
+  );
+  suggestions.innerHTML = html;
+}
+
+search.addEventListener('keyup', displayMatches);
