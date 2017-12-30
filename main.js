0f127f600cab339318d814f36af9b5a6cef3b080
Jonathan Vargas <jonathanv3232@gmail.com>
2017-12-29 19:01:05 -0500

    completes funcionality with higlighting

--- main.js
+++ main.js
@@ -14,19 +14,29 @@ function findMatches(searchTerm, data) {
   const matches = cities.filter(place => place.city.match(regex) || place.state.match(regex));
   return matches;
 }
+const numberWithCommas = x => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
 const search = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 function displayMatches() {
   const matches = findMatches(this.value, cities);
-  const html = matches.map(
-    place => `
+  const html = matches
+    .map(place => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class='hl'>${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class='hl'>${this.value}</span>`);
+      return `
     <li>
-      <span class='name'>${place.city}, ${place.state}</span>
+      <span class='name'>${cityName}, ${stateName}</span>
+      <span class='population'>${numberWithCommas(place.population)}</span>
+
     </li>
-  `,
-  );
+  `;
+    })
+    .join('');
   suggestions.innerHTML = html;
 }
 
